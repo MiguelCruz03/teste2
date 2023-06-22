930c7eaf00705f6c5d592c759b1e6fd153da87a3
MiguelCruz03 <126091723+MiguelCruz03@users.noreply.github.com>
2023-06-22 23:08:18 +0100

    Update CreateAccountScreen.java

--- pratica/pratica/src/main/java/pt/esgts/exam/cli/CreateAccountScreen.java
+++ pratica/pratica/src/main/java/pt/esgts/exam/cli/CreateAccountScreen.java
@@ -4,7 +4,8 @@ import pt.esgts.exam.exception.AccountAlreadyExists;
 import pt.esgts.exam.service.AccountService;
 
 /**
- * The screen used to create a new account
+ * Tela usada para criar uma nova conta.
+ * Extende a classe BaseScreen.
  *
  * @author Bruno Jesus
  * @version 1.0
@@ -13,23 +14,33 @@ public class CreateAccountScreen extends BaseScreen {
 
     private final AccountService accountService;
 
+    /**
+     * Construtor da tela de criação de conta.
+     *
+     * @param accountService O serviço de conta a ser utilizado.
+     */
     public CreateAccountScreen(AccountService accountService) {
         this.accountService = accountService;
     }
 
+    /**
+     * Executa a tela de criação de conta.
+     * Solicita um nome para a conta e chama o serviço para criar a conta.
+     * Exibe mensagens apropriadas dependendo do resultado da operação.
+     */
     @Override
     public void run() {
-        String accountName = askText("Choose an account name", true);
+        String accountName = askText("Escolha um nome para a conta", true);
 
         try {
             int accountId = accountService.createAccount(accountName);
-            System.out.printf("Account created with id '%d'!\n", accountId);
+            System.out.printf("Conta criada com o ID '%d'!\n", accountId);
 
         } catch (AccountAlreadyExists e) {
-            System.out.printf("Unexpected error (%s), please try again\n", e.getClass().getSimpleName());
+            System.out.printf("Erro inesperado (%s), por favor, tente novamente\n", e.getClass().getSimpleName());
         }
 
-        System.out.print("Press enter to continue...");
+        System.out.print("Pressione enter para continuar...");
         scanner.nextLine();
     }
 }
